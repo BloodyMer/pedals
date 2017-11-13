bea80068626db68edc1745782758d1dab224f0ba
Polo <BloodyMery@gmail.com>
2017-11-13 23:10:06 +0100

    Added boostrap 4 beta and removed some obsolete-by-boostrap-css

--- src/components/projects/project-row.js
+++ src/components/projects/project-row.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const ProjectRow = ({project}) =>
 	<div key={project.id}>
-    <li className="list-group-item"><Link to={'/projects/'+project.id+'/components'}>Name: {project.name}</Link></li>
+    <li className="list-group-item">Name: <Link to={'/projects/'+project.id+'/components'}>{project.name}</Link></li>
     <li className="list-group-item">Created: {project.created_at}</li>
     <li className="list-group-item">Updated: {project.updated_at}</li>
   </div>
